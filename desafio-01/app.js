dfc9c54bb011e3a70ca4aaf2dc47e8195b6d9f83
Erkmann <erkmann08@gmail.com>
2020-10-20 22:16:21 -0300

    first commit

---
+++ desafio-01/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        nome: 'Mateus',
+        idade: 19,
+        imagem: 'https://veja.abril.com.br/wp-content/uploads/2017/01/crocodilo-20170116-01-copy.jpg'
+    },
+    methods: {
+
+    }
+})
